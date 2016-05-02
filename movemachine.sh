be101ddf7e714716265ab16e2d0fa5a8a8f446f8
ffpuentes <ffpuentes@gmail.com>
2016-05-02 14:09:58 +0200

    Remove brackets

--- movemachine.sh
+++ movemachine.sh
@@ -5,8 +5,8 @@ VM_DESTINY_NAME=""
 SR_DESTINY_UUID=
 #-----------------------
 #shutdown the VM
-xe vm-shutdown uuid=${VM_ORIGINAL_UUID}
+xe vm-shutdown uuid=$VM_ORIGINAL_UUID
 #Copy the VM to the new SR and give it a name
-VM_NEW_UUID=`xe vm-copy uuid="${VM_ORIGINAL_UUID}" new-name-label="${VM_DESTINY_NAME}" sr-uuid="${SR_DESTINY_UUID}"`
+VM_NEW_UUID=`xe vm-copy uuid="$VM_ORIGINAL_UUID" new-name-label="$VM_DESTINY_NAME" sr-uuid="$SR_DESTINY_UUID"`
 #Boot the new VM
-xe vm-start uuid=${VM_NEW_UUID}
+xe vm-start uuid=$VM_NEW_UUID
--- sendToPool.sh
+++ sendToPool.sh
@@ -7,7 +7,7 @@ REMOTE_PASSWORD=
 HOST_DESTINY_UUID=
 SR_DESTINY_UUID=
 #-----------------------
-xe vm-migrate uuid=${VM_ORIGINAL_UUID} remote-master=${REMOTE_MASTER} \
-remote-username=${REMOTE_USER} remote-password=${REMOTE_PASSWORD} \
-host-uuid=${HOST_DESTINY_UUID} \
-destination-sr-uuid=${SR_DESTINY_UUID}
+xe vm-migrate uuid=$VM_ORIGINAL_UUID remote-master=$REMOTE_MASTER \
+remote-username=$REMOTE_USER remote-password=$REMOTE_PASSWORD \
+host-uuid=$HOST_DESTINY_UUID \
+destination-sr-uuid=$SR_DESTINY_UUID
